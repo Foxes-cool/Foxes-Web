fa3de135c8e0ec77282f515b7f1bf7d0bb0bf1ce
Arthur Melton <me@arthurmelton.com>
2023-07-20 23:14:11 -0400

    Got script working

---
+++ src/main.js
@@ -0,0 +1,29 @@
+{
+    var foxes = {};
+
+    let totals = new Object();
+    let tags = ["fox", "curious", "happy", "scary", "sleeping"];
+    for (let i = 0; i < tags.length; i++) {
+        eval(`
+            foxes.${tags[i]} = function() {
+                let update = false;
+                let days = Math.floor(new Date()/86400000);
+                if(totals["${tags[i]}"]) {
+                    if(Math.floor(totals["${tags[i]}"].time/86400000) != days) {
+                        update = true;
+                    }
+                }
+                else{
+                    update = true;
+                }
+                if(update) {
+                    let xmlHttp = new XMLHttpRequest();
+                    xmlHttp.open( "GET", "https://foxes.cool/counts/${tags[i]}", false );
+                    xmlHttp.send( null );
+                    totals["${tags[i]}"] = {time: days, count: xmlHttp.responseText};
+                }
+                return \`https://img.foxes.cool/${tags[i]}/\${Math.floor(Math.random()*totals["${tags[i]}"].count)}.jpg\`;
+            }
+        `);
+    }
+}
