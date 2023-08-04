17d5aec6e331ee4143fc2bd1ea594bc7785c53cb
Arthur Melton <me@arthurmelton.com>
2023-08-03 22:35:42 -0400

    Dont use eval bc websites hate it

--- src/main.js
+++ src/main.js
@@ -2,34 +2,35 @@
     var foxes = {};
 
     let totals = new Object();
-    let tags = ["fox", "curious", "happy", "scary", "sleeping"];
-    for (let i = 0; i < tags.length; i++) {
-        eval(`
-            foxes.${tags[i]} = function(object) {
-                let update = false;
-                let days = Math.floor(new Date()/86400000);
-                if(totals["${tags[i]}"]) {
-                    if(totals["${tags[i]}"].time != days) {
-                        update = true;
-                    }
-                }
-                else{
-                    update = true;
-                }
-                if(update) {
-                    let xmlHttp = new XMLHttpRequest();
-                    xmlHttp.open( "GET", "https://foxes.cool/counts/${tags[i]}", false );
-                    xmlHttp.send( null );
-                    totals["${tags[i]}"] = {time: days, count: xmlHttp.responseText};
-                }
-
-                const ret = [];
-                for (let d in object) {
-                    ret.push(d + '=' + object[d]);
-                }
-
-                return \`https://img.foxes.cool/${tags[i]}/\${Math.floor(Math.random()*totals["${tags[i]}"].count)}.jpg?\${ret.join("&")}\`.replace(/\\?$/,'');
+    function i(name, object) {
+        let update = false;
+        let days = Math.floor(new Date()/86400000);
+        if(totals[name]) {
+            if(totals[name].time != days) {
+                update = true;
             }
-        `);
+        }
+        else{
+            update = true;
+        }
+        if(update) {
+            let xmlHttp = new XMLHttpRequest();
+            xmlHttp.open( "GET", `https://foxes.cool/counts/${name}`, false );
+            xmlHttp.send( null );
+            totals[name] = {time: days, count: xmlHttp.responseText};
+        }
+
+        const ret = [];
+        for (let d in object) {
+            ret.push(d + '=' + object[d]);
+        }
+
+        return `https://img.foxes.cool/${name}/${Math.floor(Math.random()*totals[name].count)}.jpg?${ret.join("&")}`.replace(/\?$/,'');
     }
+
+    foxes.fox = function(object) {return i("fox", object)}
+    foxes.curious = function(object) {return i("curious", object)}
+    foxes.happy = function(object) {return i("happy", object)}
+    foxes.scary = function(object) {return i("scary", object)}
+    foxes.sleeping = function(object) {return i("sleeping", object)}
 }
